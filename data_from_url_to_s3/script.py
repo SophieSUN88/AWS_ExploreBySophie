f70092db87f84b487755a0872d425f90b3ba13e3
SophieSUN88 <85892147+SophieSUN88@users.noreply.github.com>
2023-07-18 18:59:07 -0500

    Upload for the test for s3

---
+++ data_from_url_to_s3/script.py
@@ -0,0 +1,21 @@
+import credential
+import boto3
+import requests
+
+object_name = "csv_data0.csv"
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id = credential.AccessKey,
+    aws_secret_access_key = credential.SecretAccessKey
+)
+
+# generate the presigned URL
+response = s3_client.generate_presigned_post(
+    Bucket = "data-from-url-sophies",
+    Key = object_name,
+    ExpiresIn = 300
+)
+
+print(response)
+
